e61a9c20d7cb1963982b25dff16dc1069bb30d6a
dariakorsunart <dariakorsunart@gmail.com>
2023-10-30 08:26:34 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const inputField = document.querySelector('.inputField');
+const btn = document.querySelector('.btn');
+const toDoContainer = document.querySelector('.toDoContainer');
+
+btn.addEventListener('click', function() {
+    const item = document. createElement('li');
+    item.innerText = inputField.value;
+    item.classList.add ('toDoAdded');
+    toDoContainer.appendChild(item);
+    inputField.value = '';
+
+    item.addEventListener('click',function(){
+    item.classList.add ('toDoCompleted')
+    })
+
+    item.addEventListener('dblclick', function(){
+    toDoContainer.removeChild(item);
+    })
+})  
+
